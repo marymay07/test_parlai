24ed3099ba07ad1c5e3f311a7fca61da64947511
Marymay <marymayezewu@yahoo.com>
2024-03-29 18:32:41 -0400

    add bucket

--- murmuration-ci/main.tf
+++ murmuration-ci/main.tf
@@ -1,19 +1,22 @@
 resource "aws_s3_bucket" "parlai" {
   bucket = "may3parl.ai"
-
+  website {
+    index_document = "index.html"
+    # error_document = "error.html"
+  }
   tags = {
     Name        = "My bucket"
     Environment = "parlai Dev"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "parlai" {
-  bucket = aws_s3_bucket.parlai.id
+# resource "aws_s3_bucket_website_configuration" "parlai" {
+#   bucket = aws_s3_bucket.parlai.id
 
-  index_document {
-    suffix = "index.html"
-  }
-}
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
 
 resource "aws_s3_bucket_ownership_controls" "parlai" {
   bucket = aws_s3_bucket.parlai.id
